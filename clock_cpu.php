c791952d94b3fd9a00dd269590255c6e7826161e
JCardenas05 <jocardenash@unal.edu.co>
2023-11-30 20:20:15 -0500

    rute

--- clock_cpu.php
+++ clock_cpu.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/json');
-$f_cpu = file_get_contents('cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq') / 1.000.000.000;
+$f_cpu = file_get_contents('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq') / 1.000.000.000;
 echo json_encode(["f_cpu" => $f_cpu]);
 ?>
