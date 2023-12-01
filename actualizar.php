e1c2adad6882fedd4861d1d4a5b4b77f4fd99925
JCardenas05 <jocardenash@unal.edu.co>
2023-12-01 08:01:08 -0500

    yes

--- actualizar.php
+++ actualizar.php
@@ -1,5 +1,5 @@
 <?php
 // Ejecutar el script de shell para actualizar el repositorio git
-exec("sudo ./var/www/Dashboard/update_git.sh");
+exec("sudo /var/www/Dashboard/./update_git.sh");
 echo "Punto de acceso reiniciado";
 ?>
