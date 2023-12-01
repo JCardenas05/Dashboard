3310a220e90967fe8a90aa538348336d40db55d0
JCardenas05 <jocardenash@unal.edu.co>
2023-11-30 23:34:50 -0500

    dwq1

--- clock_cpu.php
+++ clock_cpu.php
@@ -7,8 +7,8 @@ $freq = file_get_contents('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
 // Asegúrate de que $freq es numérico y conviértelo a un número
 //$freq = is_numeric($freq) ? floatval($freq) : 0;
 //$freq = floatval($freq)
-// Convierte la frecuencia a GHz (dividiendo por 1.000.000.000)
-$f_cpu = floatval($freq) / 1000000000;
+// Convierte la frecuencia a GHz (dividiendo por 1.000.000)
+$f_cpu = floatval($freq) / 1000000;
 
 // Devuelve los datos en formato JSON
 echo json_encode(["f_cpu" => $f_cpu]);
