35f82a0eb1c8ed8d8427de163ca844362adcdac8
JCardenas05 <jocardenash@unal.edu.co>
2023-11-30 19:30:49 -0500

    dsa

--- dispositivos.php
+++ dispositivos.php
@@ -2,38 +2,25 @@
 // Ejecuta el comando arp
 $output = shell_exec('arp -a');
 
-// Inicializa un array para almacenar los datos de ARP
-$arpData = array();
-
 // Divide la salida en líneas
 $lines = explode("\n", $output);
 
-// Recorre las líneas para procesar la información
+// Estructura para almacenar los datos procesados
+$arpEntries = [];
+
+// Procesa cada línea usando expresiones regulares
 foreach ($lines as $line) {
-    // Elimina espacios en blanco en exceso
-    $line = trim($line);
-
-    // Ignora las líneas que no contienen información relevante
-    if (empty($line) || strpos($line, 'Interface:') !== false) {
-        continue;
-    }
-
-    // Divide cada línea en partes (IP, dirección MAC, tipo)
-    $parts = preg_split('/\s+/', $line, -1, PREG_SPLIT_NO_EMPTY);
-
-    if (count($parts) == 3) {
-        // Estructura la información en un array asociativo
-        $arpEntry = array(
-            "ip" => $parts[0],
-            "mac" => $parts[1],
-            "type" => $parts[2]
-        );
-
-        // Agrega la entrada ARP al array de datos
-        $arpData[] = $arpEntry;
+    if (preg_match('/\((.*?)\)\s+at\s+(.*?)\s+\[(.*?)\]\s+on\s+(.*)/', $line, $matches)) {
+        // Agrega la información a la estructura de datos
+        $arpEntries[] = [
+            'ip' => $matches[1],
+            'mac' => $matches[2],
+            'type' => $matches[3],
+            'interface' => $matches[4]
+        ];
     }
 }
 
 // Devuelve los datos en formato JSON
-echo json_encode($arpData);
+echo json_encode($arpEntries);
 ?>
