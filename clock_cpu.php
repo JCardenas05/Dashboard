1f6bd96f13fbc8683fba8bf7a73b6546ce9cedc4
JCardenas05 <jocardenash@unal.edu.co>
2023-11-30 20:23:56 -0500

    sraras

--- clock_cpu.php
+++ clock_cpu.php
@@ -1,5 +1,15 @@
 <?php
 header('Content-Type: application/json');
-$f_cpu = file_get_contents('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq') / 1.000.000.000;
+
+// Lee la frecuencia actual de la CPU
+$freq = file_get_contents('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq');
+
+// Asegúrate de que $freq es numérico y conviértelo a un número
+$freq = is_numeric($freq) ? floatval($freq) : 0;
+
+// Convierte la frecuencia a GHz (dividiendo por 1.000.000.000)
+$f_cpu = $freq / 1000000000;
+
+// Devuelve los datos en formato JSON
 echo json_encode(["f_cpu" => $f_cpu]);
 ?>
